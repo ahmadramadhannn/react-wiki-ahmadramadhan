a263848aa522b6e235da71e50627e85105a71b9e
AhmadRamadhan-bit <ahmadoneperson@gmail.com>
2021-11-27 05:47:34 +0700

    resolve error for detail page

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,60 +1,63 @@
+import React from "react";
+import { Link } from "react-router-dom";
 import styles from "./Card.module.scss";
+import CardDetails from "./CardDetail";
 
-export default function Card({ results }) {
+const Card = ({ page, results }) => {
   let display;
 
   if (results) {
-    display = results.map((data) => {
-      const { id, image, name, status, location } = data;
+    display = results.map((x) => {
+      let { id, image, name, status, location } = x;
 
       return (
-        <div
+        <Link
+          style={{ textDecoration: "none" }}
+          to={`${page}${id}`}
           key={id}
           className="col-lg-4 col-md-6 col-sm-6 col-12 mb-4 position-relative text-dark"
         >
           <div
-            className={`${styles.card} d-flex flex-column justify-content center`}
+            className={`${styles.card} d-flex flex-column justify-content-center`}
           >
-            <img src={image} className={`${styles.img} img-fluid`} />
+            <img className={`${styles.img} img-fluid`} src={image} alt="" />
             <div className={`${styles.content}`}>
               <div className="fs-5 fw-bold mb-4">{name}</div>
               <div className="">
-                <div>
-                  <div className="fs-6 fw-normal">Last Location</div>
-                  <div className="fs-5">{location.name}</div>
-                </div>
+                <div className="fs-6 fw-normal">Last Location</div>
+                <div className="fs-5">{location.name}</div>
               </div>
             </div>
-
-            {(() => {
-              if (status === "Dead") {
-                return (
-                  <div
-                    className={`${styles.badge} position-absolute badge bg-danger`}
-                  >
-                    {status}
-                  </div>
-                );
-              } else if (status === "Alive") {
-                return (
-                  <div
-                    className={`${styles.badge} position-absolute badge bg-success`}
-                  >
-                    {status}
-                  </div>
-                );
-              } else {
-                return (
-                  <div
-                    className={`${styles.badge} position-absolute badge bg-secondary`}
-                  >
-                    {status}
-                  </div>
-                );
-              }
-            })()}
           </div>
-        </div>
+
+          {(() => {
+            if (status === "Dead") {
+              return (
+                <div
+                  className={`${styles.badge} position-absolute badge bg-danger`}
+                >
+                  {status}
+                </div>
+              );
+            } else if (status === "Alive") {
+              return (
+                <div
+                  className={`${styles.badge} position-absolute badge bg-success`}
+                >
+                  {status}
+                </div>
+              );
+            } else {
+              return (
+                <div
+                  className={`${styles.badge} position-absolute badge bg-secondary`}
+                >
+                  {status}
+                </div>
+              );
+            }
+          })()}
+        </Link>
       );
     });
   } else {
@@ -62,4 +65,6 @@ export default function Card({ results }) {
   }
 
   return <>{display}</>;
-}
+};
+
+export default Card;
